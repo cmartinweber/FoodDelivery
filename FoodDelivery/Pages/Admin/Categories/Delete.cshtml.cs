c641077e0d61c086e046f6f5679cc50e26b1b28b
Christian Martin <christianmartin@mail.weber.edu>
2025-02-21 20:07:11 -0700

    Added Razor Pages Through Scafolding

---
+++ FoodDelivery/Pages/Admin/Categories/Delete.cshtml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ApplicationCore.Models;
+using Infrastructure.Data;
+
+namespace FoodDelivery.Pages.Admin.Categories
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly Infrastructure.Data.ApplicationDbContext _context;
+
+        public DeleteModel(Infrastructure.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Category Category { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var category = await _context.Category.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Category = category;
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var category = await _context.Category.FindAsync(id);
+            if (category != null)
+            {
+                Category = category;
+                _context.Category.Remove(Category);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
