cdbf640d3218e934c4fd0f879f4374e6ebecd945
Christian Martin <christianmartin@mail.weber.edu>
2025-03-01 09:26:14 -0700

    Added Upsert Functionallity

---
+++ FoodDelivery/Pages/Admin/Categories/Upsert.cshtml.cs
@@ -0,0 +1,52 @@
+using ApplicationCore.Interfaces;
+using ApplicationCore.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FoodDelivery.Pages.Admin.Categories
+{
+    public class UpsertModel : PageModel
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        [BindProperty]
+        public Category CategoryObj { get; set; }
+
+        public UpsertModel(IUnitOfWork unitOfWork) => _unitOfWork = unitOfWork;
+
+        public IActionResult OnGet(int ? id)
+        {
+            CategoryObj = new Category();
+            if (id != 0)
+            {//edit
+                CategoryObj = _unitOfWork.Category.Get(u => u.ID == id);
+
+                if (CategoryObj == null)
+                {
+                    return NotFound();
+                }
+            }
+            return Page(); //assume insert new mode
+        }
+
+        public IActionResult OnPost()
+        {
+            if(!ModelState.IsValid)
+            {
+                return Page();
+            }
+            //If New
+            if(CategoryObj.ID == 0)
+            {
+                _unitOfWork.Category.Add(CategoryObj);
+            }
+            //existing
+            else
+            {
+                _unitOfWork.Category.Update(CategoryObj);
+            }
+            _unitOfWork.Commit();
+            return RedirectToPage("./Index");
+        }
+    }
+}
