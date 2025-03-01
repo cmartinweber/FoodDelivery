ac2f13256b8325325d9383934cc7a7106dc13698
Christian Martin <christianmartin@mail.weber.edu>
2025-02-28 18:53:06 -0700

    Create GenericRepository.cs in Data

---
+++ Infrastructure/Data/GenericRepository.cs
@@ -0,0 +1,70 @@
+﻿using ApplicationCore.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public GenericRepository(ApplicationDbContext context)
+        {
+            _dbContext = context;
+        }
+
+        public void Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(IEnumerable<T> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Get(Expression<Func<T, bool>> predicate, bool asNoTracking = false, string includes = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetAsync(Expression<Func<T, bool>> predicate, bool asNoTracking = false, string includes = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetByID(int id)
+        {
+            return _dbContext.Set<T>().Find(id);
+        }
+
+        public IEnumerable<T> List()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> List(Expression<Func<T, bool>> predicate, Expression<Func<T, int>> orderBy = null, string includes = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> ListAsync(Expression<Func<T, bool>> predicate, Expression<Func<T, int>> orderBy = null, string includes = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
