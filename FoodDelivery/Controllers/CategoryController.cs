5a1dd14c99f6a1a148c4c6eca74c40d48c1eb748
Christian Martin <christianmartin@mail.weber.edu>
2025-02-28 22:47:27 -0700

    Created CategoryController

---
+++ FoodDelivery/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using ApplicationCore.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodDelivery.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public CategoryController(IUnitOfWork unitOfWork) => _unitOfWork = unitOfWork;
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Json(new { data = _unitOfWork.Category.List() });
+        }
+    }
+}
