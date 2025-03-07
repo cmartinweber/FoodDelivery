3d065fcdaea8bdd78d0d4960f207bc2f46693ed7
Christian Martin <christianmartin@mail.weber.edu>
2025-03-07 14:57:23 -0700

    Created MenuItemController/API

--- ApplicationCore/Interfaces/IUnitOfWork.cs
+++ ApplicationCore/Interfaces/IUnitOfWork.cs
@@ -11,6 +11,7 @@ namespace ApplicationCore.Interfaces
     {
         public IGenericRepository<Category> Category { get; }
         public IGenericRepository<FoodType> FoodType { get; }
+        public IGenericRepository<MenuItem> MenuItem { get; }
 
         //save changes to data source
         int Commit();
