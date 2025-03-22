b86aed45924161009d3534dd8e3a018154e22341
Christian Martin <christianmartin@mail.weber.edu>
2025-03-21 22:09:41 -0600

    Finished User Admin Pages

--- FoodDelivery/Pages/Admin/Users/Index.cshtml.cs
+++ FoodDelivery/Pages/Admin/Users/Index.cshtml.cs
@@ -1,3 +1,6 @@
+using ApplicationCore.Models;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +8,31 @@ namespace FoodDelivery.Pages.Admin.Users
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        private readonly UnitOfWork _unitOfWork;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public IndexModel(UnitOfWork unitOfWork, UserManager<IdentityUser> userManager)
         {
+            _unitOfWork = unitOfWork;
+            _userManager = userManager;
+        }
+
+        public IEnumerable<ApplicationUser> ApplicationUsers { get; set; }
+        public Dictionary<string, List<string>> UserRoles { get; set; }
+        public bool Success { get; set; }
+        public string Message { get; set; }
+
+        public async Task OnGetAsync(bool success = false, string message = null)
+        {
+            Success = success;
+            Message = message;
+            UserRoles = new Dictionary<string, List<string>>();
+            ApplicationUsers = _unitOfWork.ApplicationUser.List();
+            foreach(var user in ApplicationUsers)
+            {
+                var userRole = await _userManager.GetRolesAsync(user);
+                UserRoles.Add(user.Id, userRole.ToList());
+            }
         }
     }
 }
